8ee137aeb9e20f732531d0343dad022175384224
AaronAP1 <aaalenort@gmail.com>
2025-08-28 13:14:39 -0500

    ssssssxx

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -57,11 +57,13 @@
                         <i class="fas fa-project-diagram me-1"></i> Flujos
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->is('ejecucion*') ? 'active' : '' }}" href="{{ route('ejecucion.index') }}">
-                        <i class="fas fa-project-diagram me-1"></i> Ejecución
-                    </a>
-                </li>
+                @if(Auth::user()->empresa && Auth::user()->empresa->editable == '1')
+                    <li class="nav-item">
+                        <a class="nav-link {{ request()->is('ejecucion*') ? 'active' : '' }}" href="{{ route('ejecucion.index') }}">
+                            <i class="fas fa-project-diagram me-1"></i> Ejecución
+                        </a>
+                    </li>
+                @endif
             @endif
 
 
