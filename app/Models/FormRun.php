dfc34e18a5d3bd380c3ebd670f3c8d931963472f
AaronAP1 <aaalenort@gmail.com>
2025-09-11 15:57:11 -0500

    sss

--- app/Models/FormRun.php
+++ app/Models/FormRun.php
@@ -8,11 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class FormRun extends Model {
     protected $table = 'form_runs';
     protected $fillable = [
-        'id_form','id_emp','id_detalle_flujo','correlativo','estado','created_by','updated_by'
+        'id_form','id_emp','id_etapas_forms','correlativo','estado','created_by','updated_by'
     ];
     public function form(){ return $this->belongsTo(Form::class,'id_form'); }
     public function empresa(){ return $this->belongsTo(Empresa::class,'id_emp'); }
-    public function detalleFlujo(){ return $this->belongsTo(DetalleFlujo::class,'id_detalle_flujo'); }
+    public function etapaForm(){ return $this->belongsTo(EtapaForm::class,'id_etapas_forms'); }
     public function answers(){ return $this->hasMany(FormAnswer::class,'id_run'); }
     public function rows(){ return $this->hasMany(FormAnswerRow::class,'id_run'); }
 }
